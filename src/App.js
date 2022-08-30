ca887deda3ad212b90dd2f1a20c36eb0214d546e
Ben Benoit <benoit.ben.j@gmail.com>
2022-08-30 13:55:13 -0500

    added app state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-// import logo from './logo.svg';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
-// import { LOCAL_API } from "./data/localAPI.js"
-// import LOCAL_API from "./data/localAPI.js"
 import LOCAL_API from "./data/localAPI.mjs"
-// import LocalAPI from "./data/localAPI.mjs"
-
 const customApiURL = LOCAL_API.getURL();
-// const customApiURL = LocalAPI.getURL();
 
 function App() {
+    const [weatherHistories, setWeatherHistories] = useState([]);
 
-  return (
-    <div className="App">
-        Welcome to my App!
-        customApiURL: {customApiURL}
-    </div>
-  );
+    const updateWeatherHistories = async () => {
+        const _weatherHistories = await fetch(LOCAL_API.getURL()).then(res => { return res.json(); })
+        setWeatherHistories(_weatherHistories);
+    }
+
+    const effectRan = useRef(false);
+
+    useEffect( () => {
+        if (!effectRan.current) {
+            effectRan.current = true;
+            updateWeatherHistories();
+        }
+    }, [])
+
+    const _getWeatherHistoryValue = (key) => {
+        return weatherHistories.map(weatherHistory => {
+            return weatherHistory[key];
+        })
+    }
+
+    const getWeatherHistoryIDs = () => {
+        return _getWeatherHistoryValue("id");
+    }
+
+    const getWeatherHistoryCities = () => {
+        return _getWeatherHistoryValue("city");
+    }
+
+    return (
+        <div className="App">
+            Welcome to my App!
+            <br/>
+            customApiURL: {customApiURL}
+            <br/>
+            getWeatherHistoryIDs: {getWeatherHistoryIDs()}
+            <br/>
+            getWeatherHistoryCities: {getWeatherHistoryCities()}
+        </div>
+    );
 }
 
 export default App;
